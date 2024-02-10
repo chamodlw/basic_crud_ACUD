b7bc801cfee37660cd2f5a6ba77febb28d143148
chamodlw <chamodweerasinghe@gmail.com>
2024-02-10 21:34:44 +0530

    init

---
+++ pro9/pro9back/app.js
@@ -0,0 +1,39 @@
+const express =require('express');
+const app =express();
+const cors =require('cors');
+const controller =require ('./controller');
+
+app.use(cors());
+
+app.use(
+    express.urlencoded({
+        extended:true,
+    })
+);
+app.use(express.json());
+
+app.get('/users',(req,res) => {
+    controller.getUsers(users =>{
+        res.send(users);
+    });
+});
+
+app.post('/createuser',(req,res) => {
+    controller.addUser(req.body, (callack) =>{
+        res.send(callack);
+    });
+});
+
+app.post('/updateuser',(req,res) => {
+    controller.updateUser(req.body, (callack) =>{
+        res.send(callack);
+    });
+});
+
+app.post('/deleteuser',(req,res) => {
+    controller.deleteUser(req.body, (callack) =>{
+        res.send(callack);
+    });
+});
+
+module.exports = app;
